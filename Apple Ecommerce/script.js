e1a50fbe1fc95205381e6aca71a98c294804b9d0
Rahul Sharma <rahulsharma15j@gmail.com>
2021-05-11 16:52:16 +0530

    controls activated

--- Apple Ecommerce/script.js
+++ Apple Ecommerce/script.js
@@ -1,3 +1,59 @@
+/**Cube */
+let x = 0;
+let y = 0;
+let z = 0;
+let bool = true;
+let interval;
+
+const cube = document.querySelector(".cube");
+
+document.querySelector(".top-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".bottom-x-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".left-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".right-y-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector(".top-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${x}deg) rotateZ(${(z -= 20)}deg)`;
+});
+
+document.querySelector(".bottom-z-control").addEventListener("click", () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${x}deg) rotateZ(${(z += 20)}deg)`;
+});
+
+const playPause = () => {
+  if (bool) {
+    setInterval(() => {
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+
+playPause();
+
+document.querySelector(".controls").addEventListener("mouseover", () => {
+  bool = false;
+  playPause();
+});
+
+document.querySelector(".controls").addEventListener("mouseout", () => {
+  bool = true;
+  playPause();
+});
+/**End of Cube */
+
 /*Slideshow*/
 const slideshowDivs = () => {
   for (i = 1; i <= 5; i++) {
